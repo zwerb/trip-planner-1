9f360c107337e6acc3c6e8006e19bea33344c4ea
Zwerb <sy@zwerb.com>
2020-11-23 11:44:01 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "YOUR API TOKEN HERE";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
